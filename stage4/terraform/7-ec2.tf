a7e8cfca844154cc7b809f0de04104f8d7e1f394
incrisz <incrisz4luv@gmail.com>
2025-02-26 13:46:59 +0100

    stage4

---
+++ stage4/terraform/7-ec2.tf
@@ -0,0 +1,37 @@
+
+resource "aws_instance" "ec2-worker1" {
+    ami = "ami-04a81a99f5ec58529"
+    instance_type = "t2.medium"
+    subnet_id = aws_subnet.public_subnet.id
+    vpc_security_group_ids = [ aws_security_group.sg_custom.id ]
+    key_name = "ipala2"
+    
+    # Tag the instance as "ec2-worker1" to differentiate it from other instances.
+    tags = {
+        "Name" = "ec2-worker1"
+    }
+  
+}
+
+
+  user_data = <<-EOF
+      #!/bin/bash
+      sudo apt update -y
+      sudo apt install -y ansible git
+
+      # Clone the Ansible playbook from GitHub
+      git clone https://github.com/Incrisz/Terraform-ansible.git /home/ubuntu/Terraform-ansible
+
+      # Run the Ansible playbook
+      cd /home/ubuntu/Terraform-ansible/ansible
+      ansible-playbook playbook.yml
+  EOF
+
+
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.ec2-worker1.public_ip
+}
+
+
